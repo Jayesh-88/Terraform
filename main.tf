1949af0ea88430aeb118e5d825dcc724465fb453
Jayesh Rewatkar <rewatkarjayesh08@gmail.com>
2026-01-14 11:44:33 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.27.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+}
+resource "aws_instance" "myserver" {
+  ami           = "ami-00ca570c1b6d79f36"
+  instance_type = "t3.nano"
+
+  tags = {
+    Name = "demoinstance1"
+  }
+}
